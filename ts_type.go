5982243a9dd7bd468b2df4578821bc54ed746975
Stephen Wan <stephen@stephenwan.com>
2023-06-09 17:17:02 -0500

    ts_type: don't emit a typecheck for unknown type
    Since the underlying app has to guard against these anyways.

--- ts_type.go
+++ ts_type.go
@@ -11,6 +11,10 @@ import (
 
 func tsTypecheckTemplate(req *plugin.CodeGenRequest, col *plugin.Column) string {
 	typ := sqliteType(req, col)
+	if typ == "unknown" {
+		return ""
+	}
+
 	cond := fmt.Sprintf(`typeof %% !== "%s"`, typ)
 	if !col.NotNull {
 		cond = fmt.Sprintf(`%s && %% !== null`, cond)
